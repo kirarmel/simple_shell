1601b0291cae557738c6f081a2e2ad74cd0570b8
Kira Armel Kayisire <kirarmel@gmail.com>
2024-08-30 02:19:59 -0700

    modified files

--- armel.c
+++ armel.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "My name is Armel"
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	printf("My name is Armel");
+	printf("My name is Armel\n");
 	return (0);
 }
